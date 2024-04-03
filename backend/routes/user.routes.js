121e8d4b6764de7c8882512ee678629ad53c3d0e
Nishant6571 <nishantbhargava.bhargava@gmail.com>
2024-04-03 20:34:46 +0530

    secret key added to env.

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -18,7 +18,7 @@ userRouter.get("/", async (req, res) => {
   }
 });
 
-// Multer configuration
+// Multer configurationb
 const storage = multer.diskStorage({});
 const upload = multer({ storage });
 
